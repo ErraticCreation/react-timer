d0d9f50e870815596c8402270733b58d1fb3bab6
ErraticCreation <ErraticCreation@gmail.com>
2016-11-03 18:25:48 -0400

    Finalized Countdown tests and added page title

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -14,7 +14,7 @@ describe('Countdown', () => {
 
   describe('handleSetCountdown', () => {
 
-    it('should set state to started and count down', (done) => {
+    it('should set state to started and countdown', (done) => {
       var countdown = TestUtils.renderIntoDocument(<Countdown/>);
       countdown.handleSetCountdown(10);
 
@@ -38,5 +38,30 @@ describe('Countdown', () => {
         done();
       }, 2001);
     });
+
+    it('should pause countdown on paused status', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(3);
+      countdown.handleStatusChange('paused');
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(3);
+        expect(countdown.state.countdownStatus).toBe('paused');
+        done();
+      },1001);
+    });
+
+    it('should reset count on stopped status', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(3);
+      countdown.handleStatusChange('stopped');
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(0);
+        expect(countdown.state.countdownStatus).toBe('stopped');
+        done();
+      },1001);
+    });
+
   });
 });
